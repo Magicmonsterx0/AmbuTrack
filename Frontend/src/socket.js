93bfc0eaa7567de70b9bcb048b6e5d15d9d1dbd9
ayush <ayushvx0@gmail.com>
2026-04-27 22:06:28 +0530

    Fix connection errors

--- Frontend/src/socket.js
+++ Frontend/src/socket.js
@@ -1,7 +1,7 @@
 import { io } from 'socket.io-client';
 
 //Single connection for whole react app shared
-const URL = import.meta.env.VITE_BACKEND_URL;
+const URL = import.meta.env.VITE_API_URL || 'http://localhost:5000';
 
 export const socket = io(URL, {
     autoConnect: true,
